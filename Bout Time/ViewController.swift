e0bc636425ff8079e1378f2c9486a11d5b3a29e6
Marcel Busschers <marcelbusschers@marcels-macbook-pro-2.local>
2017-08-30 19:59:11 +0200

    Initial Commit

---
+++ Bout Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bout Time
+//
+//  Created by Marcel Busschers on 2017/08/30.
+//  Copyright © 2017 Marcel Busschers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
