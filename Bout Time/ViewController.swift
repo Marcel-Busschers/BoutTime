f4f94b44bdb83afaf7d2878ae072ec96b1d48b05
Marcel Busschers <marcelbusschers@marcels-macbook-pro-2.local>
2017-09-07 18:24:47 +0200

    Added code for the buttons to change when selected Added outlets for the 4 event labels

--- Bout Time/ViewController.swift
+++ Bout Time/ViewController.swift
@@ -10,6 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: Outlets
+    
+    @IBOutlet weak var eventLabel1: UILabel!
+    @IBOutlet weak var eventLabel2: UILabel!
+    @IBOutlet weak var eventLabel3: UILabel!
+    @IBOutlet weak var eventLabel4: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var informationLabel: UILabel!
+    
+    // MARK: Action Buttons
+    
+    @IBAction func downFullButton(_ sender: UIButton) {
+        // TODO: Change text
+        
+        if let image: UIImage = UIImage(named: "down_full") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    @IBAction func upHalfButton1(_ sender: UIButton) {
+        // TODO: Change Text
+        
+        if let image: UIImage = UIImage(named: "up_half") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    @IBAction func downHalfButton1(_ sender: UIButton) {
+        // TODO: Change Text 
+        
+        if let image: UIImage = UIImage(named: "down_half") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    @IBAction func upHalfButton2(_ sender: UIButton) {
+        // TODO: Change Text
+        
+        if let image: UIImage = UIImage(named: "up_half") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    @IBAction func downHalfButton2(_ sender: UIButton) {
+        // TODO: Change Text
+        
+        if let image: UIImage = UIImage(named: "down_half") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    @IBAction func upFullButton(_ sender: UIButton) {
+        // TODO: Change Text
+        
+        if let image: UIImage = UIImage(named: "up_full") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    // MARK: Button Press & Change
+    
+    @IBAction func downFullTouchDown(_ sender: UIButton) {
+        if let image: UIImage = UIImage(named: "down_full_selected") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    @IBAction func upHalfTouchDown(_ sender: UIButton) {
+        if let image: UIImage = UIImage(named: "up_half_selected") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    @IBAction func downHalfTouchDown(_ sender: UIButton) {
+        if let image: UIImage = UIImage(named: "down_half_selected") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
+    @IBAction func upFullTouchDown(_ sender: UIButton) {
+        if let image: UIImage = UIImage(named: "up_full_selected") {
+            sender.setImage(image, for: .normal)
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
